7a42e8f6db1f1476d1cb9f28ecd67dd6e6bb69da
Erik Corona <ecorona@live.com>
2020-07-08 12:35:54 -0500

    Primer configuración
    Dependencias para asegurar la app inicial.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.use(helmet());
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
+  app.set('trust proxy', 1);
+
   await app.listen(3000);
 }
+
 bootstrap();
